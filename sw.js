7af3691253733637e5a8ad833d844338275620f7
Gibiamie <100677393+Gibiamie@users.noreply.github.com>
2026-07-15 19:52:29 +0400

    Add one-time cleanup for obsolete Version 03 service worker caches

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => caches.delete(key)));
+    await self.registration.unregister();
+    const windows = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    await Promise.all(windows.map(client => client.navigate(client.url)));
+  })());
+});
